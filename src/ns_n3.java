0b7f934c606503d35f4990742adbacead22bb4f7
nik2477 <sidnik2477@gmail.com>
2021-10-20 15:16:54 +0300

    Initial commit

---
+++ src/ns_n3.java
@@ -0,0 +1,14 @@
+public class ns_n3 {
+    public static void main(String[] args) {
+        double m = 8.5d;
+        double n = 11.45d;
+        double a = 10d;
+        double dis1 = a - m;
+        double dis2 = a - n;
+        if (dis1 > dis2) {
+            System.out.println(n);
+        } else {
+            System.out.println(m);
+        }
+    }
+}
